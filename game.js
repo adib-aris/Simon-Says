37717fc50948cdbb26420ab551eec483bc26ef87
Adib Aris <adibariswork@gmail.com>
2025-08-11 19:54:50 +0800

    Add function prevent zoom

--- game.js
+++ game.js
@@ -11,6 +11,7 @@ var sound;
 var userChosenColour;
 
 $(".btn").on("pointerup", firstRun);
+$(".container").on('touchstart', preventZoom);
 
 function firstRun() {
     if (level < 1) {
@@ -94,4 +95,20 @@ function checkAnswer(currentColour) {
     playSound("wrong");
     userClickPattern = 0;
   }
+}
+
+
+
+
+function preventZoom(e) {
+  var t2 = e.timeStamp;
+  var t1 = e.currentTarget.dataset.lastTouch || t2;
+  var dt = t2 - t1;
+  var fingers = e.touches.length;
+  e.currentTarget.dataset.lastTouch = t2;
+
+  if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+  e.preventDefault();
+  e.target.click();
 }
